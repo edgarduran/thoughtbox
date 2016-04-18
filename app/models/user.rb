2345589415875a703d97809fbd88220fc224e932
Edgar Duran <edgar.eduran@gmail.com>
2016-04-18 11:29:24 -0600

    user can create new account and logs in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true
+  validates :email, presence: true,
+                    uniqueness: true
   has_secure_password
   validates :password_digest, presence: true
-  validates_confirmation_of :password
 end
